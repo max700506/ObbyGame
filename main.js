ae18ae8801ea99ae523d0eece1d5a5785c55b1e1
max700506 <max.700506@gmail.com>
2025-05-06 10:47:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.body.appendChild(renderer.domElement);
 
 // === Himmel (Skybox als Hintergrund) ===
 const loader = new THREE.TextureLoader();
-loader.load('sky.jpg', function(texture) {
+loader.load('IMG_2460.jpeg', function(texture) {
 scene.background = texture;
 });
 
